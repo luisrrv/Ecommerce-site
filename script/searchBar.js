71e2f2b3a1e0c215e524582041590021f72f60a0
emanuel caro <darkemlord@gmail.com>
2021-12-26 22:27:37 +0900

    removing useless things

--- script/searchBar.js
+++ script/searchBar.js
@@ -3,9 +3,6 @@ import fetchApi from './api.js'
 const searchBarInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
 
-console.log(searchBarInput);
-console.log(searchButton);
-
 fetchApi().then((response) => {
   const newArray = response.map((element) => {
    return element.title
