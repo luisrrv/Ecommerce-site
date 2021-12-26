e104d502afce52a8469ce947d56f7ae014052cae
emanuel caro <darkemlord@gmail.com>
2021-12-26 22:15:44 +0900

    updating packages

--- script/api.js
+++ script/api.js
@@ -1,6 +1,6 @@
 const fetchApi = async () => {
   try{
-    const response = await fetch('https://fakestoreapi.com/products/1');
+    const response = await fetch('https://fakestoreapi.com/products');
     if(response.ok) {
       const jsonResponse = await response.json();
       console.log(jsonResponse)
