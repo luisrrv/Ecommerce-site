c7ba4c2cad53c5e88a5b937e388bb9e4e14467c7
emanuel caro <darkemlord@gmail.com>
2022-01-16 23:18:33 +0900

    removing useless console.log

--- public/script/searchBar.js
+++ public/script/searchBar.js
@@ -4,8 +4,8 @@ const searchBarInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
 
 fetchApi().then((response) => {
-  const newArray = response.map((element) => {
+  return response.map((element) => {
    return element.title
   });
-  console.log(newArray)
+
 });
