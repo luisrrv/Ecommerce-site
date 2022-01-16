826d98f568fa9e8f17feda676ec40087c06dc4ec
emanuel caro <darkemlord@gmail.com>
2022-01-16 21:43:14 +0900

    express server listening at port 3000

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 import express from 'express';
+
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`);
+})
