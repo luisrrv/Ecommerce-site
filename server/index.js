c8bf7e63f014316328e745b2262b497639e627cc
emanuel caro <darkemlord@gmail.com>
2022-01-16 22:50:41 +0900

    folder modificated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath  } from 'url';
+import cors from 'cors'
 
 const app = express();
 const port = 3000
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
-})
+app.use(cors())
+app.use(express.static(path.join(__dirname, "../public")))
+
 
 app.listen(port, () => {
   console.log(`server listening at http://localhost:${port}`);
