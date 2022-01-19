fc5eedd5546ca0a32c8627513c394295d62c00dc
emanuel caro <darkemlord@gmail.com>
2022-01-19 15:03:09 +0900

    server updated

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath  } from 'url';
+import dotenv from 'dotenv'
 import cors from 'cors'
 
+dotenv.config({path: ".env"})
+console.log(process.env.HOST)
 const app = express();
-const port = 3000
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, "../public")))
 
 
-app.listen(port, () => {
-  console.log(`server listening at http://localhost:${port}`);
+app.listen(process.env.HOST || 3000, () => {
+  console.log(`server listening at http://localhost:${process.env.HOST || 3000}`);
 })
