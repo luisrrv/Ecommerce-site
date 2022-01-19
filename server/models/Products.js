0ebcc8815a454b7bcc3f4438d799b86313fe2c84
emanuel caro <darkemlord@gmail.com>
2022-01-19 17:01:52 +0900

    schema created

---
+++ server/models/Products.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.ProductSchema({
+  title:{
+    type:String,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  price:{
+    type: Number,
+    require: true
+  },
+  url: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default mongoose.model('Product', productSchema);
