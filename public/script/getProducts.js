6c27322471d776458d9c20e7dbbfdca5d47564a7
emanuel caro <darkemlord@gmail.com>
2022-01-21 22:36:44 +0900

    displaying products in the ourproducts view

--- public/script/getProducts.js
+++ public/script/getProducts.js
@@ -1,3 +1,5 @@
+const productsDiv = document.getElementById('products');
+
 const fetchData = async () => {
   try{
     const response = await fetch('/products');
@@ -10,5 +12,14 @@ const fetchData = async () => {
 }
 
 fetchData().then(response => {
-  console.log(response)
+  response.map((element) => {
+    productsDiv.insertAdjacentHTML('afterbegin', `
+      <div class="products-all">
+        <h1>${element.title}</h1>
+        <img class="product-img" src="${element.url}" style="width:100px">
+        <p>${element.description}</p>
+        <span>Price: ${element.price}¥</span>
+      </div>
+    `)
+  })
 })
