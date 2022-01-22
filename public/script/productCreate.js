516c45452acf6be3ef1e61aba64bcdfd5ec73648
emanuel caro <darkemlord@gmail.com>
2022-01-22 21:09:43 +0900

    description and title input functions working

--- public/script/productCreate.js
+++ public/script/productCreate.js
@@ -1,1 +1,27 @@
-console.log('hello papis')
+const formContainer = document.getElementById('form');
+const submitButton = document.getElementById('submit-btn');
+const titleInput = document.getElementById('title');
+const descriptionInput = document.getElementById('description');
+var title = '';
+var description = '';
+
+const getTitleValue = (event) => {
+  title = event.target.value
+  console.log(title)
+}
+
+const getDescriptionValue = (event) => {
+  title = event.target.value
+  console.log(description)
+}
+
+
+titleInput.addEventListener('change', getTitleValue)
+
+
+
+const onSubmit = () => {
+  console.log('hola papis')
+}
+
+submitButton.addEventListener('click', onSubmit);
