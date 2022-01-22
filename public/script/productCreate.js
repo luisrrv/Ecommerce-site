132053e22c8d229ee784f49f8a02bd3828ee2f8b
emanuel caro <darkemlord@gmail.com>
2022-01-22 21:54:39 +0900

    const variables removed and new object created instead of

--- public/script/productCreate.js
+++ public/script/productCreate.js
@@ -4,25 +4,27 @@ const titleInput = document.getElementById('title');
 const descriptionInput = document.getElementById('description');
 const priceInput = document.getElementById('price');
 const urlInput = document.getElementById('url');
-var title = '';
-var description = '';
-var price = '';
-var url = '';
+const data = {
+  title: '',
+  description: '',
+  price: '',
+  url: ''
+}
 
 const getTitleValue = (event) => {
-  title = event.target.value
+  data.title = event.target.value
 }
 
 const getDescriptionValue = (event) => {
-  description = event.target.value
+  data.description = event.target.value
 }
 
 const getPriceValue = (event) => {
-  price = event.target.value
+  data.price = event.target.value
 }
 
 const getUrlValue = (event) => {
-  url = event.target.value
+  data.url = event.target.value
 }
 
 titleInput.addEventListener('change', getTitleValue);
@@ -31,11 +33,16 @@ priceInput.addEventListener('change', getPriceValue);
 urlInput.addEventListener('change', getUrlValue);
 
 
+
+
+// const postData = () => {
+//   fetch('/products', {
+//     method: 'POST',
+//     body: JSON.stringify()
+//   })
+// }
 const onSubmit = () => {
-  console.log(title)
-  console.log(description)
-  console.log(price)
-  console.log(url)
+  console.log(data)
   // location.reload();
 }
 
