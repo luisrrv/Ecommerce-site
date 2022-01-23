fef5b5cf8ae632fab61a4da2cf6a0ec9ec6e0797
luisrrv <rodluisr@gmail.com>
2022-01-23 22:03:54 +0900

    added buttons for the options of adding the image to the product form

---
+++ public/script/imgform.js
@@ -0,0 +1,16 @@
+function showURL() {
+  var x = document.getElementById("urlform");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function showFile() {
+  var x = document.getElementById("fileform");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
